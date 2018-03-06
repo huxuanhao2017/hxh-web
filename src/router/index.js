0da00e17669876b4352860a54a3cb0c0d97042cc
hxh <2549484862@qq.com>
2018-03-06 16:45:11 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,90 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Index from '../views/index'
+import iView from 'iview'
+
+Vue.use(Router)
+Vue.use(iView)
+export default new Router({
+  mode: 'history',
+  routes: [
+    {path: '/', redirect: '/login'},
+    {
+      path: '/heller',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/base',
+      meta: {
+        title: 'base',
+        requiresAuth: true
+      },
+      component: (resolve) => require(['../views/base/base'], resolve),
+      children: [
+        {
+          path: 'welcome',
+          name: 'welcome',
+          component: (resolve) => require(['../views/sys/welcome'], resolve),
+          meta: {
+            title: 'welcome'
+          }
+        },
+        {
+          path: 'user',
+          name: 'user',
+          // component: (resolve) => require(['./views/sys/user.vue'], resolve),
+          meta: {
+            title: 'user'
+          }
+        },
+        {
+          path: 'menu',
+          name: 'menu',
+          // component: (resolve) => require(['./views/sys/menu.vue'], resolve),
+          meta: {
+            title: 'menu'
+          }
+        },
+        {
+          path: 'role',
+          name: 'role',
+          // component: (resolve) => require(['./views/sys/role.vue'], resolve),
+          meta: {
+            title: 'role'
+          }
+        },
+        {
+          path: 'test4',
+          name: 'test4',
+          // component: (resolve) => require(['./views/test/test4.vue'], resolve),
+          meta: {
+            title: 'test4'
+          }
+        },
+        {
+          path: 'test5',
+          name: 'test5',
+          // component: (resolve) => require(['./views/test/test5.vue'], resolve),
+          meta: {
+            title: 'test5'
+          }
+        },
+        {
+          path: 'test6',
+          name: 'test6',
+          // component: (resolve) => require(['./views/test/test6.vue'], resolve),
+          meta: {
+            title: 'test6'
+          }
+        }
+      ]
+    }
+  ]
+})
---
+++ src/views/sys/welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="welcome">
+    <img src="../../images/welcome2.jpg"/>
+  </div>
+</template>
+<script>export default {}
+</script>
+<style lang="stylus" ref="stylesheet/stylus">
+  .welcome
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    overflow: auto;
+    background-position: center center;
+    box-shadow: 0 0px 3px rgba(0, 0, 0, .5);
+    text-align: center;
+    .welcome img
+      width: 100%
+      height: 100%
+      overflow: auto
+</style>
